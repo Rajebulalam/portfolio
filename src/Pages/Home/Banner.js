6cfb17fa988de1cc4a1fd21a9438f88b0cd101be
unknown <rajebul.alam17@gmail.com>
2022-06-09 22:41:43 +0600

    Typing Animation Implement

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import banner from '../../images/banner.jpg';
+import Person from '../../images/person.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <section style={{ background: `url(${banner})`, backgroundSize: 'cover' }}>
+        <section className='py-14' style={{ background: `url(${banner})`, backgroundSize: 'cover' }}>
             <div className='w-full sm:w-11/12 mx-auto px-4 sm:px-6'>
-                <h2>Banner</h2>
+                <div className='flex items-center justify-center'>
+                    <img className='w-[130px] border-4 rounded-full' src={Person} alt="personIamge" />
+                </div>
+                <h2 style={{ fontFamily: 'Merienda' }} className='text-white flex items-center justify-center py-3 pt-8 text-4xl font-extrabold'>I'm <span className='animation text-green-500 pl-3 uppercase'> Md. Rajebul Alam Khokan</span> </h2>
             </div>
         </section>
     );
